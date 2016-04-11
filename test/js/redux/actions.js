ea98ecddfde79600e73d9d93347068fbe4ab40ff
Douglas Rocha <douglasbellonrocha@gmail.com>
2016-04-10 23:20:24 -0300

    Fixed lack of attention todo to contact

--- test/js/redux/actions.js
+++ test/js/redux/actions.js
@@ -8,31 +8,31 @@ describe('actions', () => {
   it('should create an action to add a todo', () => {
     const contact = { name: "John", phone: "555-5555" };
     const expectedAction = {
-      type: constants.addTodo,
+      type: constants.addContact,
       contact
     }
 
-    expect(actions.addTodo(contact)).toEqual(expectedAction);
+    expect(actions.addContact(contact)).toEqual(expectedAction);
   });
 
   it('should create an action to edit a todo', () => {
     const id = 1;
     const contact = { id, name: "John", phone: "555-5555"};
     const expectedAction = {
-      type: constants.editTodo,
+      type: constants.editContact,
       contact
     }
 
-    expect(actions.editTodo(contact)).toEqual(expectedAction);
+    expect(actions.editContact(contact)).toEqual(expectedAction);
   });
 
   it('should create an action to remove a todo', () => {
     const id = 3;
     const expectedAction = {
-      type: constants.removeTodo,
+      type: constants.removeContact,
       id
     }
 
-    expect(actions.removeTodo(id)).toEqual(expectedAction);
+    expect(actions.removeContact(id)).toEqual(expectedAction);
   });
 });
